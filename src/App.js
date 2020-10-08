71592752342c2bff6c494a50ba09eea938d61c16
Lee <lsy63681@naver.com>
2020-10-08 19:38:05 +0900

    Shooping first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shooping</a>
+      </header>
+      <main>product List</main>
+      <footer>All right reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
