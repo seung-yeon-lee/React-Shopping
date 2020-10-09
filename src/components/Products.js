96011dc80b9dbfbb530f7d0c81fe1de14f1ea4e6
Lee <lsy63681@naver.com>
2020-10-09 19:04:56 +0900

    Create Product Component

---
+++ src/components/Products.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { formats } from "../util";
+
+export default class Products extends Component {
+  render() {
+    const { products } = this.props;
+    return (
+      <div>
+        <ul className="products">
+          {products.map((product) => (
+            <li key={product.id}>
+              <div className="product">
+                <a href={"#" + product._id}>
+                  <img src={product.image} alt={product.title} />
+                  <p>{product.title}</p>
+                </a>
+                <div className="product-price">
+                  <div>{formats(product.price)}</div>
+                  <button className="button button-primary">
+                    장바구니에 담기
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
