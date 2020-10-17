43eeb1f44690759416fa78f994bae61cedd7a15a
Lee <lsy63681@naver.com>
2020-10-17 20:54:49 +0900

    add redux-FilterComponent

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -1,4 +1,4 @@
-import { FETCH_PRODUCTS } from "../type";
+import { FETCH_PRODUCTS, FILTER_PRICE, FILTER_SIZE } from "../type";
 
 export const fetchProducts = () => async (dispatch) => {
   const res = await fetch("/api/products");
@@ -6,5 +6,42 @@ export const fetchProducts = () => async (dispatch) => {
   dispatch({
     type: FETCH_PRODUCTS,
     payload: data,
+  });
+};
+
+export const filterProducts = (products, size) => (dispatch) => {
+  dispatch({
+    type: FILTER_SIZE,
+    payload: {
+      size: size,
+      items:
+        size === ""
+          ? products
+          : products.filter((x) => x.size.indexOf(size) >= 0),
+    },
+  });
+};
+
+export const sortProducts = (products, sort) => (dispatch) => {
+  const sortedProducts = products.slice();
+  if (sort === "init") {
+    sortedProducts.sort((a, b) => (a.title > b.title ? 1 : -1));
+  } else {
+    sortedProducts.sort((a, b) =>
+      sort === "lowest"
+        ? a.price > b.price
+          ? 1
+          : -1
+        : a.price > b.price
+        ? -1
+        : 1
+    );
+  }
+  dispatch({
+    type: FILTER_PRICE,
+    payload: {
+      sort: sort,
+      items: sortedProducts,
+    },
   });
 };
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,32 +1,71 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { filterProducts, sortProducts } from "../actions/productAction";
 
-export default class Filter extends Component {
+class Filter extends Component {
+  sortProducts = (e) => {
+    const { filteredProducts, sortProducts } = this.props;
+    const {
+      target: { value },
+    } = e;
+    sortProducts(filteredProducts, value);
+  };
+
+  filterProducts = (e) => {
+    const {
+      target: { value },
+    } = e;
+    const { filterProducts, products } = this.props;
+    filterProducts(products, value);
+  };
+
   render() {
-    const { count, size, sort, filterProducts, sortProducts } = this.props;
+    const { size, sort, filteredProducts } = this.props;
+
     return (
-      <div className="filter">
-        <div className="filter-result">{count}개의 상품이있습니다</div>
-        <div className="filter-sort">
-          정렬{" "}
-          <select value={sort} onChange={sortProducts}>
-            <option>최신상품</option>
-            <option value="lowest">낮은가격순</option>
-            <option value="highest">높은가격순</option>
-          </select>
-        </div>
-        <div className="filter-size">
-          사이즈{" "}
-          <select value={size} onChange={filterProducts}>
-            <option value="">ALL</option>
-            <option value="XS">XS</option>
-            <option value="S">S</option>
-            <option value="M">M</option>
-            <option value="L">L</option>
-            <option value="XL">XL</option>
-            <option value="XXL">XXL</option>
-          </select>
-        </div>
-      </div>
+      <>
+        {!filteredProducts ? null : (
+          <div className="filter">
+            <div className="filter-result">
+              {filteredProducts.length}개의 상품이있습니다
+            </div>
+            <div className="filter-sort">
+              정렬{" "}
+              <select value={sort} onChange={this.sortProducts}>
+                <option value="init">최신상품</option>
+                <option value="lowest">낮은가격순</option>
+                <option value="highest">높은가격순</option>
+              </select>
+            </div>
+            <div className="filter-size">
+              사이즈{" "}
+              <select onChange={this.filterProducts}>
+                <option value="">ALL</option>
+                <option value="XS">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+                <option value="XXL">XXL</option>
+              </select>
+            </div>
+          </div>
+        )}
+      </>
     );
   }
 }
+const mapStateToProps = (state) => {
+  const size = state.product.size;
+  const sort = state.product.sort;
+  const products = state.product.items;
+  const filteredProducts = state.product.filteredItems;
+  console.log(products, filteredProducts);
+  return { size, sort, products, filteredProducts };
+};
+const mapDispatchToProps = {
+  filterProducts,
+  sortProducts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
